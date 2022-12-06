6e78d6ba5407709ec9cdd02b924c67be4cf718eb
Gabriel Frischknecht <gf@nexplore.ch>
2022-12-06 07:53:04 +0100

    Day 6

---
+++ AdventOfCode.Test/Day6Tests.cs
@@ -0,0 +1,36 @@
+﻿using System.Text.RegularExpressions;
+using AdventOfCode.Days;
+using NUnit.Framework;
+
+namespace AdventOfCode.Test;
+
+public class Day6Tests
+{
+    [TestCase("mjqjpqmgbljsphdztnvjfqwrcgsmlb", 7)]
+    [TestCase("bvwbjplbgvbhsrlpgdmjqwftvncz", 5)]
+    [TestCase("nppdvjthqldpwncqszvftbrmjlhg", 6)]
+    [TestCase("nznrnfrfntjfmvfwmzdfjlvtqnbhcprsg", 10)]
+    [TestCase("zcfzfwzzqfrljwzlrfnpqdbhtmscgvjw", 11)]
+    public void Day6Part1Tests(string input, int ouptut)
+    {
+        var day6Solver = new CodeDay6();
+        var result = day6Solver.Solve(input);
+        Assert.AreEqual(ouptut, result);
+    }
+    [TestCase("mjqjpqmgbljsphdztnvjfqwrcgsmlb", 19)]
+    [TestCase("bvwbjplbgvbhsrlpgdmjqwftvncz", 23)]
+    [TestCase("nppdvjthqldpwncqszvftbrmjlhg", 23)]
+    [TestCase("nznrnfrfntjfmvfwmzdfjlvtqnbhcprsg", 29)]
+    [TestCase("zcfzfwzzqfrljwzlrfnpqdbhtmscgvjw", 26)]
+    public void Day6Part2Tests(string input, int ouptut)
+    {
+        var day6Solver = new CodeDay6
+        {
+            PacketSize = 14
+        };
+
+        var result = day6Solver.Solve(input);
+        Assert.AreEqual(ouptut, result);
+    }
+    
+}
---
+++ AdventOfCode/Days/CodeDay6.cs
@@ -0,0 +1,27 @@
+﻿namespace AdventOfCode.Days;
+
+public class CodeDay6
+{
+    public int PacketSize = 4;
+    public int Solve(string input)
+    {
+        var inputPacket = input.ToCharArray().ToList();
+
+        for (var i = 0; i < inputPacket.Count; i++)
+        {
+            var fourLetters = inputPacket.GetRange(i, PacketSize);
+            if (AllfourLettersAreUnique(fourLetters))
+            {
+                return i + PacketSize;
+            }
+        }
+        
+        throw new ArgumentException("No solution found");
+    }
+
+    private bool AllfourLettersAreUnique(List<char> fourLetters)
+    {
+        var uniqueLetters = fourLetters.Distinct();
+        return uniqueLetters.Count() == fourLetters.Count();
+    }
+}
