0facfb29232b6beacbf539e3d4072bc2af2ea0aa
gf@nexplore.ch <gf@nexplore.ch>
2025-12-06 19:08:45 +0100

    optimized a bit

--- Python/2025/Day6.py
+++ Python/2025/Day6.py
@@ -35,29 +35,9 @@ for elem in dictionary :
     
 print(totalSum)
         
-# part 2
-dictionary = {}
-        
-class CephaCalculation :
-    def __init__(self, index):
-        self.values = { 0: "", 1: "", 2: "", 3: ""}
-        self.operator = ''
-        self.index = index
-        
-    def calculate(self) :
-        if self.operator == '*':
-            result = 1
-            for value in self.values :
-                if(self.values[value]):
-                    result *= int(self.values[value])
-            return result
-        elif self.operator == '+' :
-            return sum(self.values)
-        
-            
+# part 2                  
 rows = {}
 operators = {}
-print("part 2")
 for line in lines :
     for index, char in enumerate(list(line)) :
         if char.isnumeric() :
@@ -65,38 +45,33 @@ for line in lines :
                 rows[index] = ''
             rows[index] += char
         elif char.strip() :
-            operators[index] = char.strip()
+            operators[index] = char.strip() == '*'
 sumPerOperator = {}
-index = 0
 operators = list(operators.values())
-
-print(operators)
 previousKey = 0
 operatorIndex = 0
 rows = dict(sorted(rows.items()))
-print(rows)
 
 for row in rows :
-    if row > previousKey+1 :
+    if row > previousKey + 1 :
         operatorIndex += 1
     previousKey = row
     currentOp = operators[operatorIndex]
-    isMult = currentOp == '*'
     
     if operatorIndex not in sumPerOperator :
-        if isMult :
+        if currentOp :
             sumPerOperator[operatorIndex] = 1
         else :
             sumPerOperator[operatorIndex] = 0
     
-    if isMult :
+    if currentOp :
         sumPerOperator[operatorIndex] *= int(rows[row])
     else :
         sumPerOperator[operatorIndex] += int(rows[row])
     
-print("sum", sumPerOperator)    
 totalSum = 0
 for operatorSum in sumPerOperator :
     totalSum += sumPerOperator[operatorSum]
     
 print(totalSum)
+print(11494432585168)
